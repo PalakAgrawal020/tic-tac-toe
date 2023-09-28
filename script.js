17cbbebca2023612cc241a948413f04388bf5b96
Palak <palakagrawal020@gmail.com>
2023-09-28 15:15:24 +0530

    Render the elements of the Array using JS

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const GameBoard = [] //use modules
-const Players = [] //create with factories
+const GameBoard = (() => {
+    const board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+    
+    const getBoard = () => board;
+    
+    return { getBoard };
+})(); 
 
-const cells = document.getElementsByClassName('cell')
+const Players = (name,symbol) => {
+    return {name, symbol}
+} 
+
+const gameboard = document.querySelector('.board');
+const boardArray = GameBoard.getBoard();
+
+boardArray.forEach(symbol => {
+    const newCell = document.createElement('div');
+    newCell.textContent = symbol;
+    newCell.style.border = '1px solid gray';
+    newCell.style.padding = '30px';
+    gameboard.appendChild(newCell);
+});
+ 
