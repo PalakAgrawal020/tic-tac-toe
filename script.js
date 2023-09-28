d0afb79a801b385c9a3c035413fb90486550d3d2
Palak <palakagrawal020@gmail.com>
2023-09-28 17:20:14 +0530

    Add the functionality to input names of players and start button to start game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const GameBoard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
     const getBoard = () => board;
@@ -5,67 +6,75 @@ const GameBoard = (() => {
     return { getBoard };
 })();
 
-const Player = (name, symbol) => {
-    return { name, symbol };
-};
+let players = [];
 
-const players = [
-    Player("Player1", "X"),
-    Player("Player2", "O")
-];
-
-let currentPlayerIndex = 0; 
+let currentPlayerIndex = 0;
 let gameEnd = false;
 
 const gameboard = document.querySelector('.board');
 const boardArray = GameBoard.getBoard();
 
-for (let i = 0; i < 9; i++) {
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     
-    const newCell = document.createElement('div');
-    newCell.classList.add('cell');
-    newCell.textContent = boardArray[i];
-    gameboard.appendChild(newCell);
+    const name1 = document.getElementById('name1').value;
+    const name2 = document.getElementById('name2').value;
 
-    newCell.addEventListener('click', () => {
-        if(gameEnd) {
-            return;
-        }
+    players = [
+        { name: name1, symbol: "X" },
+        { name: name2, symbol: "O" }
+    ];
 
-        if (boardArray[i] === "") {
-            const currentPlayer = players[currentPlayerIndex];
-            newCell.textContent = currentPlayer.symbol;
-            boardArray[i] = currentPlayer.symbol;
+    name1.value = "";
+    name2.value = "";
 
-            currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+    for (let i = 0; i < 9; i++) {
 
-            // Check for a winner or tie
-            if (winner()) {
-                showWinner(currentPlayer);
-                gameEnd = true;
-            } else if (tie()) {
-                showTie();
-                gameEnd = true;
+        const newCell = document.createElement('div');
+        newCell.classList.add('cell');
+        newCell.textContent = boardArray[i];
+        gameboard.appendChild(newCell);
+
+        newCell.addEventListener('click', () => {
+            if (gameEnd) {
+                return;
             }
-        }
-    });
-}
+
+            if (boardArray[i] === "") {
+                const currentPlayer = players[currentPlayerIndex];
+                newCell.textContent = currentPlayer.symbol;
+                boardArray[i] = currentPlayer.symbol;
+
+                currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+
+                if (winner()) {
+                    showWinner(currentPlayer);
+                    gameEnd = true;
+                } else if (tie()) {
+                    showTie();
+                    gameEnd = true;
+                }
+            }
+        });
+    }
+});
 
 function winner() {
     const winPatterns = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-        [0, 4, 8], [2, 4, 6] 
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ];
-    
+
     for (const pattern of winPatterns) {
         const [a, b, c] = pattern;
         if (boardArray[a] && boardArray[a] === boardArray[b] && boardArray[a] === boardArray[c]) {
-            return true; 
+            return true;
         }
     }
-    
-    return false; 
+
+    return false;
 }
 
 function tie() {
@@ -85,14 +94,12 @@ function showTie() {
     alert("It's a tie!");
 }
 
-
-const restart = document.querySelector('button');
+const restart = document.querySelector('.restart-btn');
 restart.addEventListener('click', () => {
+    name1.value = "";
+    name2.value = "";
     gameEnd = false;
+    gameboard.innerHTML = '';
 
     boardArray.fill("");
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
-        cell.textContent = "";
-    });
 });
